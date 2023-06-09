02c8ba932cb9fcb92fc42b7587d53a9c3898f100
Jonny Campbell <hi@jonnycampbell.com>
2023-06-09 13:05:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   src="https://chat.dante-ai.com/embed?kb_id=5a5268d1-8e86-4c12-b078-3e557e332f7e&token=5332028d-639c-442d-b33c-09f09811af4f&modeltype=gpt-4&logo=ZmFsc2U=&mode=false"
   allow="clipboard-write; microphone"
   width="920"
-  height="1200"
+  height="900"
   frameborder="0"
   ></iframe>
       </main>
