7c3efbe1167d9895daaf352afc323740062f1ff8
Jonny Campbell <hi@jonnycampbell.com>
2023-06-09 13:02:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+<iframe
+  src="https://chat.dante-ai.com/embed?kb_id=5a5268d1-8e86-4c12-b078-3e557e332f7e&token=5332028d-639c-442d-b33c-09f09811af4f&modeltype=gpt-4&logo=ZmFsc2U=&mode=false"
+  allow="clipboard-write; microphone"
+  width="920"
+  height="1200"
+  frameborder="0"
+  ></iframe>
       </main>
 
       <Footer />
